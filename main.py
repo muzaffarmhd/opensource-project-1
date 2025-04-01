a17c8b1e212f0a68fc7e03e52dfca06709041aee
Muzaffar <mdmuzaffarprof@gmail.com>
2025-04-01 17:21:35 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def calculator():
+	num1 = float(input("Enter first number: "))
+	op = input("Enter operator (+, -, *, /): ")
+	num2 = float(input("Enter second number: "))
+	if op == "+":
+		return num1 + num2
+	elif op == "-":
+		return num1 - num2
+	elif op == "*":
+		return num1 * num2
+	elif op == "/":
+		return "Error! Division by zero." if num2 == 0 else num1 / num2
+	else:
+		return "Invalid operator!"
+
+print("Result:", calculator())
+
+
+
